0bb8e7eb91a55173e85cfdd76f536ec90ac36d7f
Oliver Tansley <84975755+OliverTansley@users.noreply.github.com>
2024-06-18 18:02:11 +0100

    angled robot other way changed delta time

--- Course_backend/Robot.py
+++ Course_backend/Robot.py
@@ -7,10 +7,13 @@ class EmptyRobot:
   
   def __init__(self) -> None:
     self.laser = Laser()
+    # Robots position
     self.x:float = 2
     self.y:float =  8
-    self.orientation:float = 0 # degrees
-
+    self.orientation:float = math.pi # degrees
+    # Robots velocity
+    self.linear_vel = 0
+    self.angular_vel = 0
 
   def show(self,ax) -> None:
     self.draw_circle_with_radius(ax,(self.x,self.y),0.4,self.orientation)
@@ -21,15 +24,15 @@ class EmptyRobot:
     ax.add_patch(self.circle)
 
     # Calculate the end point of the radius line based on the angle
-    angle_radians = math.pi * angle_degrees/180 
-    x_end = center[0] + radius * math.sin(angle_radians)
-    y_end = center[1] + radius * math.cos(angle_radians)
+
+    x_end = center[0] + radius * math.sin(self.orientation)
+    y_end = center[1] + radius * math.cos(self.orientation)
     
     # Draw the radius line
     ax.plot([center[0], x_end], [center[1], y_end], color='black')
 
   def run(self) -> Tuple[float,float]:
-    return (0,2.25)
+    return (0,1.5)
 
     
 class Laser:
--- Course_backend/Simulation.py
+++ Course_backend/Simulation.py
@@ -9,10 +9,10 @@ from matplotlib.animation import FuncAnimation
 
 
 global DELTA_TIME
-DELTA_TIME = 100
+DELTA_TIME = 50
 
 
-def simulate_robot(robot:EmptyRobot,robot_map:List[Wall] = base_map,total_time = 15, noise_strength = 0.1) -> FuncAnimation:
+def simulate_robot(robot:EmptyRobot,robot_map:List[Wall] = base_map,total_time = 10, noise_strength = 0.1) -> FuncAnimation:
   
   robot.x,robot.y = 2,8
   
@@ -29,21 +29,21 @@ def simulate_robot(robot:EmptyRobot,robot_map:List[Wall] = base_map,total_time =
     
     # Update environment
     wheel_speeds: Tuple[float, float] = robot.run()
-    left_wheel_change = wheel_speeds[0] * DELTA_TIME/1000 
-    right_wheel_change = wheel_speeds[1] * DELTA_TIME/1000 
+    left_wheel_change = wheel_speeds[0] * (DELTA_TIME *10**-3)
+    right_wheel_change = wheel_speeds[1] * (DELTA_TIME *10**-3)
     
     average_change = (left_wheel_change + right_wheel_change)/2
-    bearing_change = (right_wheel_change - left_wheel_change)/0.4
+    bearing_change = (right_wheel_change - left_wheel_change)/0.8
     
-    robot.orientation += 360 * bearing_change/2*math.pi # robot orientation is stored in degrees
+    robot.orientation += bearing_change # robot orientation is stored in degrees
     
     if bearing_change != 0:
       R = average_change/bearing_change
-      delta_x = R * (math.cos(2*math.pi*robot.orientation/360) - math.cos(2*math.pi*robot.orientation/360 - 360 * bearing_change/2*math.pi)) + random()/10 * noise_strength
-      delta_y = -R * (math.sin(2*math.pi*robot.orientation/360) - math.sin(2*math.pi*robot.orientation/360 - 360 * bearing_change/2*math.pi)) + random()/10 * noise_strength
+      delta_x = -R * (math.cos(robot.orientation) - math.cos(robot.orientation - bearing_change)) + random()/10 * noise_strength
+      delta_y = R * (math.sin(robot.orientation) - math.sin(robot.orientation - bearing_change)) + random()/10 * noise_strength
     else:
-      delta_x = average_change*math.cos(2*math.pi*robot.orientation/360 - 360 * bearing_change/2*math.pi) + random()/10 * noise_strength
-      delta_y = average_change*math.sin(2*math.pi*robot.orientation/360 - 360 * bearing_change/2*math.pi) + random()/10 * noise_strength
+      delta_x = average_change*math.cos(robot.orientation - bearing_change) + random()/10 * noise_strength
+      delta_y = average_change*math.sin(robot.orientation - bearing_change) + random()/10 * noise_strength
     
     robot.x += delta_x
     robot.y += delta_y
@@ -54,3 +54,8 @@ def simulate_robot(robot:EmptyRobot,robot_map:List[Wall] = base_map,total_time =
   sim_animation = FuncAnimation(fig=fig,func=update_sim, frames=int(total_time * int(1000/DELTA_TIME)),interval=int(DELTA_TIME))
   return sim_animation
   
+  
+if __name__ == "__main__":
+  task1_robot:EmptyRobot = EmptyRobot()
+  anim: FuncAnimation = simulate_robot(robot=task1_robot, noise_strength=0)
+  plt.show()
